d9a14bb7bfa654f1eade9dc92fb0856a11e2a399
Hui Yun <huiyun2@alumni.uv.es>
2026-05-18 23:43:05 +0200

    fix(lab0): remove TODO placeholder from blink_once

--- sandbox/blink.c
+++ sandbox/blink.c
@@ -37,7 +37,6 @@ void blink_init(void)
  */
 void blink_once(unsigned int delay_ms)
 {
-    /* TODO (Ejercicio B): implement on branch feat/blink-led */
     /* set PB7 high */
     /* wait delay_ms cycles */
     /* set PB7 low */
